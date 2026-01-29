461b5c565bf3af5d62a96f17f564836cdc70f790
Devil-Aura <imh203356@gmail.com>
2026-01-29 19:46:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+
+if os.path.exists("downloads"):
+    for f in os.listdir("downloads"):
+        try:
+            os.remove(os.path.join("downloads", f))
+        except:
+            pass
 from bot import TrackRemoverBot
 
 if __name__ == "__main__":
